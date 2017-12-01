104db507b429e339ccd1b34116b50d8673d53828
tondi <mtondos2@gmail.com>
2017-12-01 09:33:28 +0100

    Moves drag logic to DragService

--- src/app/components/note/note.controller.js
+++ src/app/components/note/note.controller.js
@@ -2,67 +2,11 @@ class NoteController {
   constructor($scope, $element, $document, DragService) {
     'ngInject';
     this.dragService = DragService;
-    this.test = this.dragService.onTouchStart();
+    this.dragService.init($element, $scope);
+
+    // this.test = this.dragService.onTouchStart();
     // eslint-disable-next-line
-    console.log(this.test);
-
-    const element = $element;
-
-    let startX = 0;
-    let startY = 0;
-    let x = 0;
-    let y = 0;
-
-    element.css({
-      position: 'absolute',
-      cursor: 'pointer'
-    });
-
-    element.on('mousedown', event => {
-      $scope.$emit('note/clicked', {el: $element});
-
-      event.preventDefault();
-      startX = event.pageX - x;
-      startY = event.pageY - y;
-      $document.on('mousemove', mousemove);
-      $document.on('mouseup', mouseup);
-    });
-
-    function mousemove(event) {
-      y = event.pageY - startY;
-      x = event.pageX - startX;
-      element.css({
-        transform: `translate(${x}px, ${y}px)`
-      });
-    }
-
-    function mouseup() {
-      $document.off('mousemove', mousemove);
-      $document.off('mouseup', mouseup);
-    }
-
-    element.on('touchstart', event => {
-      $scope.$emit('note/clicked', {el: $element});
-
-      event.preventDefault();
-      startX = event.changedTouches[0].pageX - x;
-      startY = event.changedTouches[0].pageY - y;
-      $document.on('touchmove', touchmove);
-      $document.on('touchend', touchend);
-    });
-
-    function touchmove(event) {
-      y = event.changedTouches[0].pageY - startY;
-      x = event.changedTouches[0].pageX - startX;
-      element.css({
-        transform: `translate(${x}px, ${y}px)`
-      });
-    }
-
-    function touchend() {
-      $document.off('touchmove', touchmove);
-      $document.off('touchend', touchend);
-    }
+    // console.log(this.test);
   }
 }
 
--- src/app/shared/drag/drag.service.js
+++ src/app/shared/drag/drag.service.js
@@ -1,17 +1,72 @@
 class DragService {
-  constructor($log) {
+  constructor($log, $document) {
     this.$log = $log;
-    // $element.bind('mousedown', this.onTouchStart);
+    this.$document = $document;
   }
 
-  onTouchStart() {
-    this.$log.log('darg srv fired');
-  }
+  init(el, $scope) {
+    this.el = el;
+    this.scope = $scope;
 
-  // static DragServiceFactory($element, $log) {
-  //   return new DragService($element, $log);
-  // }
+    const element = this.el;
+    let startX = 0;
+    let startY = 0;
+    let x = 0;
+    let y = 0;
+
+    element.css({
+      position: 'absolute',
+      cursor: 'pointer'
+    });
+
+    element.on('mousedown', event => {
+      this.scope.$emit('note/clicked', {el});
+
+      event.preventDefault();
+      startX = event.pageX - x;
+      startY = event.pageY - y;
+      this.$document.on('mousemove', mousemove);
+      this.$document.on('mouseup', mouseup);
+    });
+
+    const mousemove = event => {
+      y = event.pageY - startY;
+      x = event.pageX - startX;
+      element.css({
+        transform: `translate(${x}px, ${y}px)`
+      });
+    };
+
+    const mouseup = () => {
+      this.$document.off('mousemove', mousemove);
+      this.$document.off('mouseup', mouseup);
+    };
+
+    element.on('touchstart', event => {
+      this.scope.$emit('note/clicked', {el});
+
+      event.preventDefault();
+      startX = event.changedTouches[0].pageX - x;
+      startY = event.changedTouches[0].pageY - y;
+      this.$document.on('touchmove', touchmove);
+      this.$document.on('touchend', touchend);
+    });
+
+    const touchmove = event => {
+      y = event.changedTouches[0].pageY - startY;
+      x = event.changedTouches[0].pageX - startX;
+      element.css({
+        transform: `translate(${x}px, ${y}px)`
+      });
+    };
+
+    const touchend = () => {
+      this.$document.off('touchmove', touchmove);
+      this.$document.off('touchend', touchend);
+    };
+  }
 }
+
 
 // DragService.$inject = ['$element', '$log'];
 
